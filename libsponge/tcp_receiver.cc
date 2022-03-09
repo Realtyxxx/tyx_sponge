ce0d7a278e4126ec805f2b9004dd34c057fd85a9
realtyxxx <841344563@qq.com>
2022-03-09 14:10:41 +0800

    remove print

--- libsponge/tcp_receiver.cc
+++ libsponge/tcp_receiver.cc
@@ -1,7 +1,5 @@
 #include "tcp_receiver.hh"
 
-#include <iostream>
-
 // Dummy implementation of a TCP receiver
 
 // For Lab 2, please replace with a real implementation that passes the
@@ -31,16 +29,17 @@ void TCPReceiver::segment_received(const TCPSegment &seg) {
     //     _ackno = _seqno + _payload.length() + seg.header().syn + seg.header().fin;
     size_t offset = int(!seg.header().syn);
     size_t index = unwrap(_seqno, _isn.value(), static_cast<uint64_t>(stream_out().bytes_written())) - offset;
+
     _reassembler.push_substring(_payload, index, seg.header().fin);
+
     uint64_t first_unassembled = static_cast<uint64_t>(stream_out().bytes_read() + stream_out().buffer_size());
-    cout << "index : " << index << " "
-         << "payload " << _payload << "\n";
-    printf("first_unassembled : %lu\n", first_unassembled);
+    /* cout << "index : " << index << " "
+     *      << "payload " << _payload << "\n";
+     * printf("first_unassembled : %lu\n", first_unassembled);
+     * printf("unassembled : %lu\n", unassembled_bytes()); */
     _ackno = wrap(first_unassembled + 1 +
                       (_fin.has_value() && first_unassembled  + 1>= unwrap(_fin.value(), _isn.value(), first_unassembled)),
                   _isn.value());
-    // seg.header().syn + seg.header().fin;
-    printf("unassembled : %lu\n", unassembled_bytes());
 }
 
 optional<WrappingInt32> TCPReceiver::ackno() const { return _ackno; }
